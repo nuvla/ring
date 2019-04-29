367249d079c2d8c3594400e693aa006821a56ef1
Charles Loomis <cal@sixsq.com>
2019-04-29 09:23:27 +0200

    release 2.0.0

--- code/project.clj
+++ code/project.clj
@@ -1,6 +1,6 @@
 (def parent-version "6.5.0")
 
-(defproject sixsq.nuvla.ring/code "2.0.0-SNAPSHOT"
+(defproject sixsq.nuvla.ring/code "2.0.0"
 
   :description "simple ring container for micro-services"
 
