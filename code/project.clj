a4330701c78f4c0fb5d9753d3f555ad18879dae4
khaled basbous <khaled@sixsq.com>
2019-11-20 15:17:02 +0100

    next development version

--- code/project.clj
+++ code/project.clj
@@ -1,6 +1,6 @@
 (def parent-version "6.7.0")
 
-(defproject sixsq.nuvla.ring/code "2.0.4"
+(defproject sixsq.nuvla.ring/code "2.0.5-SNAPSHOT"
 
   :description "simple ring container for micro-services"
 
