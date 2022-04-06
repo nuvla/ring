e44aeb6682710cc756e2a7c68c722eb09d9a12a1
khaled basbous <khaled@sixsq.com>
2022-04-06 16:25:55 +0200

    release 2.0.8

--- code/project.clj
+++ code/project.clj
@@ -1,6 +1,6 @@
 (def parent-version "6.7.11")
 
-(defproject sixsq.nuvla.ring/code "2.0.8-SNAPSHOT"
+(defproject sixsq.nuvla.ring/code "2.0.8"
 
   :description "simple ring container for micro-services"
 
